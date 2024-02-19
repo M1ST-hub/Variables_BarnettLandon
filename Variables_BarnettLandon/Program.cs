387b7adb4d5006fd724e8ebc6d7258751e1a23f9
M1ST-hub <nmckrypt@gmail.com>
2024-02-19 11:00:21 -0600

    Initial commit

---
+++ Variables_BarnettLandon/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Variables_BarnettLandon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   //This ihh my favorite silly goofy funtime number
+            int favoriteNumber = 69;
+            //idk why you want to know if I'm runnin, I'm obviously sittin
+            bool isJumping = false; bool isRunning = false;
+            //Another favorite number
+            float notSure = 5.9f;
+            //Give me a 100 :sunglasses_emoji:
+            const double finalGrade = 100;
+
+            //print to console
+            Console.WriteLine(favoriteNumber);
+            Console.WriteLine(isJumping);
+            Console.WriteLine(isRunning);
+            Console.WriteLine(notSure);
+            Console.WriteLine(finalGrade);
+        }
+    }
+}
